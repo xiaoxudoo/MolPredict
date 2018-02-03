458221f9b1d6baa902e2aa555aa014b8c27d596c
xiaoxudong [肖旭东] <xiaoxudong@xiaomi.com>
2018-02-03 17:25:56 +0800

    build-2018-02-03-17-25-56

--- routes/index.js
+++ routes/index.js
@@ -8,21 +8,20 @@ router.get('/', function(req, res, next) {
   res.setHeader("Pragma", "no-cache"); // HTTP 1.0.
   res.setHeader("Expires", "0"); // Proxies.
   res.render('drug/pc/about');
-  // res.redirect('/moltox')
 });
 
 // pkmol
 
-router.get('/moltox', drugController.prediction)
-router.get('/moltox/about', drugController.theory)
-router.get('/moltox/run_example', drugController.run_example)
-router.post('/moltox/silicotox', drugController.formSubmit_absorption);
+router.get('/moltox', toxController.prediction)
+router.get('/moltox/about', toxController.theory)
+router.get('/moltox/run_example', toxController.run_example)
+router.post('/moltox/silicotox', toxController.formSubmit_absorption);
 
-router.get('/molopt', drugController.prediction)
-router.get('/molopt/about', drugController.theory)
-router.get('/molopt/run_example', drugController.run_example)
-router.post('/molopt/silicotox', drugController.formSubmit_absorption);
-router.post('/molopt/silicoopt', drugController.formSubmit_optimisation);
+router.get('/molopt', optController.prediction)
+router.get('/molopt/about', optController.theory)
+router.get('/molopt/run_example', optController.run_example)
+router.post('/molopt/silicotox', optController.formSubmit_absorption);
+router.post('/molopt/silicoopt', optController.formSubmit_optimisation);
 
 
 module.exports = router;
